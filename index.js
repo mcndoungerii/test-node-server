53779952d5cd2bbc387f88140bd7d3dca898d11d
Allen Mwandunga <allenmcndounger@gmail.com>
2022-04-08 00:49:07 +0300

    initial commit

--- index.js
+++ index.js
@@ -37,4 +37,44 @@ app.get("/items", (req, res) => {
     ]
   });
 });
+
+//GET ONE
+app.get("/item/:id", (req, res) => {
+  const { id } = req.params;
+  const { no } = req.query;
+  if (!Number(no)) res.status(401).send({ message: "Please provide the no" });
+  if (!Number(id)) res.status(400).send({ message: "We want id" });
+
+  res.status(200).send({ message: `This is item ${id}` });
+});
+
+//POST
+app.post("/item", (req, res) => {
+  const { name } = req.body;
+  if (!name) res.status(401).send({ message: "Please provide name" });
+
+  res.status(201).send({ message: `${name} is saved successfully` });
+});
+
+//UPDATE
+app.patch("/item/:id", (req, res) => {
+  const { id } = req.params;
+  const { name } = req.body;
+
+  if (!id) res.status(401).send({ message: `Please provide id` });
+
+  if (!name) res.status(401).send({ message: `We want name` });
+
+  res.status(201).send({ message: `The name ${name} is updated sucessfully` });
+});
+
+//DELETE
+
+app.delete("/item/:id", (req, res) => {
+  const { id } = req.params;
+  if (!id) res.status(401).send({ message: "Please provide an id" });
+
+  res.status(201).send({ message: `id ${id} deleted successfully` });
+});
+
 app.listen(PORT, () => console.log(`Server is listen at port ${PORT}`));
